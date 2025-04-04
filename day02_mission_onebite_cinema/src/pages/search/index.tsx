0825b6a2395dd36ea195584c1a9c31a787554b4c
DKULena <32173057@dankook.ac.kr>
2025-04-04 21:38:18 +0900

    add: day5_mission

--- day02_mission_onebite_cinema/src/pages/search/index.tsx
+++ day02_mission_onebite_cinema/src/pages/search/index.tsx
@@ -1,17 +1,19 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router"
+// import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import movies from "@/mock/dummy.json";
+import MovieItem from "@/components/movie-item";
 
 export default function Page() {
-    const router = useRouter();
-    console.log(router)
-    const { q } = router.query;
-
-    return (
-        <h2>검색결과 : {q}</h2>
-    )
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieItem className="movie_search_item" key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
-    return <SearchableLayout>{page}</SearchableLayout>
-}
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
