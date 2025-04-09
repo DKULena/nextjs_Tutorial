fe0bf4f81f1d22b95daba3543c673e1076bc4545
DKULena <32173057@dankook.ac.kr>
2025-04-09 22:28:52 +0900

    add: fallback

--- day02_mission_onebite_cinema/src/pages/movie/[id].tsx
+++ day02_mission_onebite_cinema/src/pages/movie/[id].tsx
@@ -5,6 +5,8 @@ import {
 } from "next";
 import style from "./[id].module.css";
 import fetchOneMovie from "@/lib/fetchOneMovie";
+import { useRouter } from "next/router";
+import { notFound } from "next/navigation";
 
 export const getStaticPaths = () => {
   return {
@@ -13,13 +15,19 @@ export const getStaticPaths = () => {
       { params: { id: "2" } },
       { params: { id: "3" } },
     ],
-    fallback: false,
+    fallback: true,
   };
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const movie = await fetchOneMovie(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { movie },
@@ -30,7 +38,11 @@ export default function Page(
   // { movie }: InferGetServerSidePropsType<typeof getServerSideProps>
   { movie }: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩중입니다.";
   if (!movie) return "문제가 발생했습니다 다시 시도하세요";
+
   const {
     id,
     title,
