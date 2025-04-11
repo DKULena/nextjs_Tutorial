4139556ab8cee5f00d5f60c4138a74ff80d45c6f
DKULena <32173057@dankook.ac.kr>
2025-04-11 22:16:01 +0900

    add: seo optimization

--- section_02/src/pages/index.tsx
+++ section_02/src/pages/index.tsx
@@ -5,9 +5,9 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-book";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
-  console.log("index page");
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -25,6 +25,13 @@ export default function Home({
   recoBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요" />
+      </Head>
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
@@ -38,7 +45,7 @@ export default function Home({
           <BookItem key={book.id} {...book} />
         ))}
       </section>
-    </div>
+    </div></>
   );
 }
 
